641a1a86004757b2c456285f042ace64d8073ddc
mtriSE <mtri0162@gmail.com>
2023-12-07 17:04:47 +0700

    edit /services/authorization.php to only return json

--- services/authorization.php
+++ services/authorization.php
@@ -4,11 +4,16 @@ session_start();
 include '../config/httpConfig.config.php';
 
 if (!isset($_SESSION['id'])) {
-    header('Location: /public/login/index.html');
+    $data = array(
+        "status_code" => 401,
+        "status_message" => getStatusCode(401)
+    );
+    // header('Location: /public/login/index.html');
 } else {
     $data = array(
         "status_code" => 200,
         "status_message" => getStatusCode(200)
     );
-    echo json_encode($data);
 }
+
+echo json_encode($data);
