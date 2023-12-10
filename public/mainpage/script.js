c406907160d171d29fcd348591a02c61e428ce29
Đậu Đức Quân <bruceparkerblue@gmail.com>
2023-12-10 15:41:57 +0700

    Validate date

---
+++ public/mainpage/script.js
@@ -0,0 +1,22 @@
+// Get the input elements
+let manufacturingDateInput = document.getElementById("manufacture");
+let expiryDateInput = document.getElementById("expiry");
+let today = new Date();
+
+// Add an event listener to the input elements
+manufacturingDateInput.addEventListener("change", validateDates);
+expiryDateInput.addEventListener("change", validateDates);
+
+function validateDates() {
+  let manufacturingDate = new Date(manufacturingDateInput.value);
+  let expiryDate = new Date(expiryDateInput.value);
+
+  if (manufacturingDate > expiryDate) {
+    alert("The manufacturing date must be earlier than the expiry date");
+  }
+
+  if (expiryDate < today) {
+    alert("Product already or about to be expired");
+    return;
+  }
+}
