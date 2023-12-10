5d2e70e4770365d44bbb86f489a89f4fc1aa1a3f
mtriSE <mtri0162@gmail.com>
2023-12-10 18:00:39 +0700

    add login feature

--- public/login/login.js
+++ public/login/login.js
@@ -1,9 +1,33 @@
-$(document).ready(function() {
-    $("#showpwd").click(function() {
-        if($(this).is(":checked")) {
-            $("#inputPassword").attr("type", "text");
-        } else {
-            $("#inputPassword").attr("type", "password");
-        }
-    });
+var test;
+$("#submit-btn").click(function (event) {
+    event.preventDefault();
+    console.log(JSON.stringify({
+        inputUserName: $("#intputUsername").val(),
+        inputPassword: $("#inputPassword").val()
+    }));
+    fetch('http://localhost/services/authentication.php', {
+        method: 'POST',
+        credentials: "include",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            inputUserName: $("#intputUsername").val(),
+            inputPassword: $("#inputPassword").val()
+        })
+    })
+        .then(response => response)
+        .then(json => {
+     
+            switch (json.status) {
+                case 200:
+                    window.location = "http://localhost/public/mainpage/mainpage.html";
+                    break;
+                case 404:
+                case 406:
+                    $("#noti").text("Thông tin đăng nhập sai");
+                    break;
+            }
+
+        })
 });
